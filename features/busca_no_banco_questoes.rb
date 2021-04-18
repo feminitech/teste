a59dc14b2a56174ff6b40f4cad5c39818bbeea97
Nayara <nayara@feminitech.com.br>
2021-04-18 00:49:51 -0300

    automaçao inicial

--- features/busca_no_banco_questoes.rb
+++ features/busca_no_banco_questoes.rb
@@ -2,17 +2,19 @@ require 'selenium-webdriver'
 driver = Selenium::WebDriver.for :chrome
 
 Dado('que navego para a página de busca do banco de questões') do
-    pending # Write code here that turns the phrase above into concrete actions
+    driver.navigate.to "https://opentdb.com/browse.php"
   end
   
   Dado('digito {string} no campo de busca') do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+    driver.find_element(:name, 'query').send_keys("Ciência: Computadores")
   end
   
   Quando('clico no botão de buscar') do
-    pending # Write code here that turns the phrase above into concrete actions
+    driver.find_element(:xpath, '//*[@id="page-top"]/div[1]/form/div/button').click
   end
   
   Então('visualizo uma mensagem de erro com o texto {string}') do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+    mensagem_esperada = driver.find_element(:css, "#page-top > div:nth-child(3) > div")
+        "No questions found".eql? mensagem_esperada.text
+    sleep 5
   end
